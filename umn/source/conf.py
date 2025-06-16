c92dab113154a763dd2c39ecfb20b38073c94c0f
Sebastian Gode <sebastian.gode@telekom.de>
2025-06-16 09:48:38 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- umn/source/conf.py
+++ umn/source/conf.py
@@ -38,7 +38,10 @@ otcdocs_doc_type = 'umn'
 otcdocs_service_category = 'storage'
 otcdocs_service_title = 'Cloud Backup and Recovery'
 otcdocs_service_type = 'cbr'
+otcdocs_service_environment = 'public'
+otcdocs_cloud_environment = 'eu_de'
 otcdocs_search_environment = 'hc_de'
+otcdocs_search_index = 'search_index_de'
 otcdocs_search_url = "https://opensearch.eco.tsi-dev.otc-service.com/"
 
 # If extensions (or modules to document with autodoc) are in another directory,
